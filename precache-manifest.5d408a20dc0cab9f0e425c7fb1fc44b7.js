3de12057f2f69b2a6aff13b22182d92b58fbb51f
lavana555 <timofeevniki3@gmail.com>
2020-02-23 19:32:35 +0300

    Updates

---
+++ precache-manifest.5d408a20dc0cab9f0e425c7fb1fc44b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b52cb613e9517f6bb8808d673cde6e",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "63b16d12efa8288b7b68",
+    "url": "/calculator/static/css/main.3dd56967.chunk.css"
+  },
+  {
+    "revision": "3a70d494ff987a714117",
+    "url": "/calculator/static/js/2.4805a260.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.4805a260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b16d12efa8288b7b68",
+    "url": "/calculator/static/js/main.4d44e122.chunk.js"
+  },
+  {
+    "revision": "13734abc995ab99d9932",
+    "url": "/calculator/static/js/runtime-main.c91ea19c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
