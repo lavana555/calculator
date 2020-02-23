b75e954e529ee325bc1b81ef09bceb966eef95e5
lavana555 <timofeevniki3@gmail.com>
2020-02-23 18:43:41 +0300

    step2

--- src/App.js
+++ src/App.js
@@ -13,44 +13,52 @@ class App extends React.Component {
 
     }
     onChangedNumberInput = (newcounter) => {
+       // let string=`${this.state.string}`
+        if (this.state.counter.toString().slice(0) != '0') {
+
+
         let number = this.state.counter + `${newcounter}`
         this.setState({
             counter: number
         })
     }
+    }
     onChangeOperationsInput = (operation) => {
-
+//debugger;
+//console.dir(this.state.counter)
         if (operation === "C") {
             this.setState({
                 counter: '',
             })
         } else if (operation === "<") {
-            let counter = this.state.counter.toString(this.state.counter)
+          //  let newcounter2 = `${this.state.counter}`
+            let counter = this.state.counter.toString()
             let newcounter = counter.substring(0, counter.length - 1)
 
             this.setState({
                 counter: newcounter
             })
         } else if (operation === "=") {
-            let blockoperations=["/","*","-","+"]
-            if (!blockoperations.includes(this.state.counter.slice(-1))
-            )  {
+            let blockoperations = ["/", "*", "-", "+"]
+                //let counter = `${this.state.counter}`
+
+            if (!blockoperations.includes(this.state.counter.toString().slice(-1))) {
                 let newcounter = eval(this.state.counter)
                 this.setState({
                     counter: newcounter
                 })
             }
         } else {
-            let blockoperations=["/","*","-","+"]
+            let blockoperations = ["/", "*", "-", "+"]
 
-            if (!blockoperations.includes(this.state.counter.slice(-1))) {
+            if (!blockoperations.includes(this.state.counter.toString().slice(-1))) {
 
-            let counter = this.state.counter + `${operation}`
-            this.setState({
-                counter: counter,
-                operation: counter
-            })
-        }
+                let counter = this.state.counter + `${operation}`
+                this.setState({
+                    counter: counter,
+                    operation: counter
+                })
+            }
         }
     }
 
@@ -60,7 +68,7 @@ class App extends React.Component {
                 <div className="calc-body">
                     <div className="calc-screen">
                         <div className="calc-typed">
-                        <ValueBlock counter={this.state.counter}/>
+                            <ValueBlock counter={this.state.counter}/>
                         </div>
                     </div>
                     <div className="calc-button-row">
